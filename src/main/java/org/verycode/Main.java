8d1ca4e56ee98250de62e13a5676ace0df468795
wanghp <wanghp@wanghpdeMacBook-Pro.local>
2023-04-21 15:59:16 +0800

    Initial commit

---
+++ src/main/java/org/verycode/Main.java
@@ -0,0 +1,11 @@
+package org.verycode;
+
+/**
+ * @Author wanghuaping
+ * @Date ${DATE} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
